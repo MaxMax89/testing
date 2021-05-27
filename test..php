21b4597bf5b37c585e2a657dce84264cc8e3a1e7
MaxMax89 <chip_pro89@mail.ru>
2021-05-27 22:11:05 +0300

    add string_1, string_2 and echo

---
+++ test..php
@@ -0,0 +1,15 @@
+<meta charset="UTF-8">
+<?php
+$my_string = 'Простая строка';
+$my_string2 = "еще одна строка";
+
+$count = 13;
+
+$string_1 = "В строке \"Hello, world!\" $count символов.<br>";
+$string_1 = 'В строке \'Hello, world!\' $count символов.<br>';
+
+echo $string_1;
+echo $string_2;
+
+
+?>
--- test.php
+++
@@ -1,7 +0,0 @@
-<meta charset="UTF-8">
-<?php
-$my_string = 'Простая строка'
-$my_string2 = "еще одна строка"
-
-
-?>
