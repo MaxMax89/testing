b25ceff98977b4592301238034081f66e1ece999
MaxMax89 <chip_pro89@mail.ru>
2021-06-01 21:59:19 +0300

    Подстановка значений с использованием фигурных скобок

--- test..php
+++ test..php
@@ -11,5 +11,15 @@ $string_1 = 'В строке \'Hello, world!\' $count символов.<br>';
 echo $string_1;
 echo $string_2;
 
+$sport1 = 'волей';
+$sport2 = 'фут';
+$sport3 = 'баскет';
 
+$play1 = "Я люблю играть в {$sport1}бол.<br>";
+$play2 = "Я люблю играть в {$sport2}бол.<br>";
+$play3 = "Я люблю играть в {$sport3}бол.<br>";
+
+echo $play1;
+echo $play2;
+echo $play3;
 ?>
